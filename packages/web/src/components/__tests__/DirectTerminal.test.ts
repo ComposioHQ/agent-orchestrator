Pritom14/agent-orchestrator-go

fix(web): extract safeFit helper, skip stale resize on failure
Address review feedback: when fit.fit() throws, skip sending stale
terminal dimensions to the server. Extract a testable safeFit() helper
that returns false on failure, allowing callers to bail out before
sending resize messages with outdated cols/rows.

Add unit tests for safeFit to meet diff coverage requirements.

Co-Authored-By: Claude Opus 4.6 (1M context) <noreply@anthropic.com>

packages/web/src/components/__tests__/DirectTerminal.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "vitest";
-import { buildDirectTerminalWsUrl, buildTerminalThemes } from "@/components/DirectTerminal";
+import { buildDirectTerminalWsUrl, buildTerminalThemes, safeFit } from "@/components/DirectTerminal";
+import type { FitAddon as FitAddonType } from "@xterm/addon-fit";
 
 describe("buildDirectTerminalWsUrl", () => {
   it("keeps non-standard port when proxy path override is set", () => {
@@ -145,5 +146,19 @@ describe("buildTerminalThemes", () => {
   it("selection colors differ between dark and light themes", () => {
     const { dark, light } = buildTerminalThemes("agent");
     expect(dark.selectionBackground).not.toBe(light.selectionBackground);
+  });
+});
+
+describe("safeFit", () => {
+  it("returns true when fit() succeeds", () => {
+    const addon = { fit: () => {} } as unknown as FitAddonType;
+    expect(safeFit(addon)).toBe(true);
+  });
+
+  it("returns false when fit() throws (dimensions not ready)", () => {
+    const addon = {
+      fit: () => { throw new TypeError("Cannot read properties of undefined (reading 'dimensions')"); },
+    } as unknown as FitAddonType;
+    expect(safeFit(addon)).toBe(false);
   });
 });

ComposioHQ/agent-orchestrator

packages/web/src/components/__tests__/DirectTerminal.test.ts
@@ -1,5 +1,6 @@
 import { afterEach, beforeEach, describe, it, expect } from "vitest";
-import { buildTerminalThemes, resolveMonoFontFamily } from "@/components/DirectTerminal";
+import { buildTerminalThemes, resolveMonoFontFamily, safeFit } from "@/components/DirectTerminal";
+import type { FitAddon as FitAddonType } from "@xterm/addon-fit";
 
 const HEX_RE = /^#[0-9a-fA-F]{6}$/;
 const ANSI_KEYS = [
@@ -118,5 +119,19 @@ describe("resolveMonoFontFamily", () => {
       "__JetBrains_Mono_abc123",
     );
     expect(resolveMonoFontFamily()).not.toMatch(/var\(/);
+  });
+});
+
+describe("safeFit", () => {
+  it("returns true when fit() succeeds", () => {
+    const addon = { fit: () => {} } as unknown as FitAddonType;
+    expect(safeFit(addon)).toBe(true);
+  });
+
+  it("returns false when fit() throws (dimensions not ready)", () => {
+    const addon = {
+      fit: () => { throw new TypeError("Cannot read properties of undefined (reading 'dimensions')"); },
+    } as unknown as FitAddonType;
+    expect(safeFit(addon)).toBe(false);
   });
 });